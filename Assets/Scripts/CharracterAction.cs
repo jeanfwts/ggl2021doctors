36d925b18aa0c7c56ac6f2cebf787e4487e0d4f2
Simon Binet <binetsimon@gmail.com>
2021-01-29 15:20:09 +0100

    ajout du lancer de player

---
+++ Assets/Scripts/CharracterAction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharracterAction : MonoBehaviour
+{
+    //public GameObject crosshairs;
+    Vector3 target;
+    public GameObject prefabLamp;
+    public GameObject cam;
+
+    public float bulletSpeed = 60.0f;
+
+    Rigidbody2D player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+     {
+         target = cam.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+         //crosshairs.transform.position = new Vector2(target.x, target.y);
+
+         Vector3 difference = target - player.transform.position;
+         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+         
+
+         if (Input.GetMouseButtonDown(0))
+         {
+             Debug.Log(target.y +" " + target.x);
+             float distance = difference.magnitude;
+             Vector2 direction = difference / distance;
+             direction.Normalize();
+             fireLight(direction, rotationZ);
+         }
+
+     }
+    
+    void fireLight(Vector2 direction, float rotationZ)
+    {
+        GameObject light = Instantiate(prefabLamp) as GameObject;
+        light.transform.position = player.transform.position;
+        light.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        light.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+    }
+}
---
+++ Assets/Scripts/Light.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    public float TimeToLive = 5f;
+    public GameObject player;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, TimeToLive);
+        player = GameObject.FindGameObjectWithTag("player");
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+}
