359e812c54de3d7dca697aacff3fcb593ebdbc8b
jeanfwts <weets.jeanfrancois@gmail.com>
2021-01-29 23:14:08 +0100

    monstre a loupiote

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // la torche s'allume au contact du projectile
+        if (collision.GetComponent<Collider2D>().tag == "Player")
+        {
+            Destroy(_player);
+        }
+     
+    }
+}
